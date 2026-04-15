8c8ea254167932ab36558336c229b6aeb6037333
past13 <laurynas.janilionis@gmail.com>
2026-04-15 19:17:01 +0300

    update command

---
+++ Command/Program.cs
@@ -0,0 +1,38 @@
+﻿using Command.Command2;
+using CommandPattern.Command2;
+using Receiver = Command.Command1.Receiver;
+
+namespace Command
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var receiver = new Receiver();
+            var command1 = new Command1.Command(receiver);
+
+            command1.Execute();
+            command1.Redo();
+            command1.Undo();
+            command1.Execute();
+            
+            ICommand command = new CommandExample2(new Receiver1());
+            command.Execute();
+            command.Redo();
+            command.Undo();
+            command.Set("III");
+            command.Execute();
+
+            Console.WriteLine();
+            
+            ICommand command2 = new CommandExample1(new Receiver2());
+            command2.Set("houses ");
+            command2.Execute();
+            
+            command2.Set("castles ");
+            command2.Undo();
+            command2.Execute();
+            command2.Redo();
+        }
+    }
+}
