a71bd31a8cd0d7b47f0a8c3c3fbab529c5942475
past13 <laurynas.janilionis@gmail.com>
2026-04-20 18:31:12 +0300

    update email

--- Mediator/Program.cs
+++ Mediator/Program.cs
@@ -6,19 +6,9 @@ namespace Mediator
     {
         static void Main()
         {
-            Email.Mediator m = new Email.Mediator();
-            // Two from head office and one from a branch office
-            Colleague head1 = new Colleague(m, "John");
-            ColleagueB branch1 = new ColleagueB(m, "David");
-            Colleague head2 = new Colleague(m, "Lucy");
-
-            head1.Send("Meeting on Tuesday, please all ack");
-            branch1.Send("Ack"); // by design does not get a copy
-            m.Block(branch1.Receive); // temporarily gets no messages
-            head1.Send("Still awaiting some Acks");
-            head2.Send("Ack");
-            m.Unblock(branch1.Receive); // open again
-            head1.Send("Thanks all");
+            
+            
+            Email.Email.Run();
         }
     }
 }
